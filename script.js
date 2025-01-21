6623a1360ff22452aadd596f6ac2aef44871ee10
Anees Ul Haq <aneesulhaq935@gmail.com>
2025-01-21 19:32:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+console.log("salam")
+
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+for(tablink of tablinks){
+    tablink.classList.remove("active-link");
+}
+
+for(tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab");
+}
+
+event.currentTarget.classList.add("active-link");
+
+document.getElementById(tabname).classList.add("active-tab");
+
+}  
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+    console.log("ok ha bhi");
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+    console.log("ok");
+}
+
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyyRurImVXac1Aj71aIAJaUS1qjLn2_V-W6QsJ8lLFeyeGFkGURO5tYWshowVs1y0iR/exec'
+const form = document.forms['submit-to-google-sheet']
+
+const msg= document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML="Message Sent Successfully";
+        setTimeout(function() {
+            msg.innerHTML = " ";
+        }, 4000);
+        form.reset();
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
+
+
+
+
+
